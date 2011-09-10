317dcdef595a8ca947d1a4b58090211be349db11
Paul Cavallaro <paulcavallaro@gmail.com>
2011-09-10 12:30:44 -0400

    Added support for character classes and escaping

--- HGlob.hs
+++ HGlob.hs
@@ -10,6 +10,9 @@ matchesPattern text pattern
 
 matchesPattern' :: String -> GlobPattern -> Bool
 matchesPattern' (c:cs) ('?':ps)         = matchesPattern' cs ps
+matchesPattern' (c:cs) ('\\':p:ps)
+                | c == p                = matchesPattern' cs ps
+                | otherwise             = False
 matchesPattern' str@(c:cs) pat@('*':ps) = or [(matchesPattern' cs ps),
                                               (matchesPattern' cs pat),
                                               (matchesPattern' str ps)]
@@ -24,27 +27,26 @@ matchesPattern' "" "*"          = True
 matchesPattern' _ _             = False
 
 charClass :: Char -> GlobPattern -> (Bool, GlobPattern)
-charClass x ('[':'!':c:cs)       = let (elems, remPat) = charClass' [c] cs
+charClass x pat@('[':'!':cs)       = let (elems, remPat) = charClass' "" pat
                                  in (not (x `elem` elems), remPat)
-charClass x ('[':c:cs)           = let (elems, remPat) = charClass' [c] cs
+charClass x pat@('[':cs)           = let (elems, remPat) = charClass' "" pat
                                  in (x `elem` elems, remPat)
 charClass x _                    = error "Invalid Character Class"
 
 charClass' :: String -> GlobPattern -> (String, GlobPattern)
+charClass' es ('[':'!':cs)  = charClass' es cs
+charClass' es ('[':cs)      = charClass' es cs
 charClass' es (']':cs)  = (es, cs)
+charClass' es (n:'-':m:cs) = charClass' ([n..m] ++ es) cs
+charClass' es ('\\':c:cs) = charClass' (c:es) cs
 charClass' es (c:cs)    = charClass' (c:es) cs
 charClass' _ _          = error "Unterminated Character Class"
+
+alphabet = ['a'..'z']
 
 {-- validPattern: Check to see if the provided GlobPattern is a valid one --}
 validPattern :: GlobPattern -> Bool
 validPattern _ = True
-{--
-validPattern "" = True
-validPattern ('*':cs) = True
-validPattern ('[':cs) = True
-validPattern ('?':cs) = validPattern cs
-validPattern (c:cs)   = validPattern cs
---}
 
 validCharClass :: GlobPattern -> Bool
 validCharClass _ = True
